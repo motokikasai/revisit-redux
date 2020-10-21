62420282e8f8f0325623caf5907bcabc6122de9d
motokikasai <kmomoky@hotmail.com>
2020-10-21 16:24:58 +0200

    Clean & replace Counter to To-do reducers

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
-import Button from './components/Buttons';
+// import Button from './components/Buttons';
 
 function App() {
   return (
     <div className='App'>
-      <h1 style={{ color: '#ad5a5a' }}>Redux Typical Counter</h1>
-      <Button />
+      {/* <h1 style={{ color: '#ad5a5a' }}>Redux Typical Counter</h1> */}
+      {/* <Button /> */}
+      <h1>Redux Typical To-do App!</h1>
     </div>
   );
 }
--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,62 +1,62 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+// import React, { Component } from 'react';
+// import { connect } from 'react-redux';
 
-class Buttons extends Component {
-  handleIncrement = () => {
-    this.props.dispatch({ type: 'INCREMENT' });
-  };
+// class Buttons extends Component {
+//   handleIncrement = () => {
+//     this.props.dispatch({ type: 'INCREMENT' });
+//   };
 
-  handleDecrement = () => {
-    this.props.dispatch({ type: 'DECREMENT' });
-  };
+//   handleDecrement = () => {
+//     this.props.dispatch({ type: 'DECREMENT' });
+//   };
 
-  render() {
-    return (
-      <div>
-        <button
-          style={{
-            fontSize: '2rem',
-            width: '40px',
-            background: '#5a2e08',
-            border: 'none',
-            borderRadius: '6px',
-            color: 'white',
-          }}
-          onClick={this.handleDecrement}
-        >
-          -
-        </button>
-        <span
-          style={{
-            display: 'inline-block',
-            fontSize: '2rem',
-            fontWeight: '600',
-            color: '#5a2e08',
-            width: '100px',
-          }}
-        >
-          {this.props.count}
-        </span>
-        <button
-          style={{
-            fontSize: '2rem',
-            width: '40px',
-            background: '#5a2e08',
-            border: 'none',
-            borderRadius: '6px',
-            color: 'white',
-          }}
-          onClick={this.handleIncrement}
-        >
-          +
-        </button>
-      </div>
-    );
-  }
-}
+//   render() {
+//     return (
+//       <div>
+//         <button
+//           style={{
+//             fontSize: '2rem',
+//             width: '40px',
+//             background: '#5a2e08',
+//             border: 'none',
+//             borderRadius: '6px',
+//             color: 'white',
+//           }}
+//           onClick={this.handleDecrement}
+//         >
+//           -
+//         </button>
+//         <span
+//           style={{
+//             display: 'inline-block',
+//             fontSize: '2rem',
+//             fontWeight: '600',
+//             color: '#5a2e08',
+//             width: '100px',
+//           }}
+//         >
+//           {this.props.count}
+//         </span>
+//         <button
+//           style={{
+//             fontSize: '2rem',
+//             width: '40px',
+//             background: '#5a2e08',
+//             border: 'none',
+//             borderRadius: '6px',
+//             color: 'white',
+//           }}
+//           onClick={this.handleIncrement}
+//         >
+//           +
+//         </button>
+//       </div>
+//     );
+//   }
+// }
 
-const mapStateToProps = state => ({
-  count: state.countReducer.count, // combineReducer must be taken into account!
-});
+// const mapStateToProps = state => ({
+//   count: state.countReducer.count, // combineReducer must be taken into account!
+// });
 
-export default connect(mapStateToProps)(Buttons);
+// export default connect(mapStateToProps)(Buttons);
