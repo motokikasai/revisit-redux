b708571e5d90bb78055dc8dc6363640ff35871ee
motokikasai <kmomoky@hotmail.com>
2020-10-21 16:46:09 +0200

    Test w/o React

--- src/index.js
+++ src/index.js
@@ -4,9 +4,28 @@ import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
-import rootReducer from './reducers';
+import todoApp from './reducers';
 
-const store = createStore(rootReducer);
+import {
+  addTodo,
+  toggleTodo,
+  setVisibilityFilter,
+  VisibilityFilters,
+} from './actions/actions';
+
+const store = createStore(todoApp);
+
+const unsubscribe = store.subscribe(() => console.log(store.getState()));
+
+// console.log(store.getState());
+store.dispatch(addTodo('Learn about actions'));
+store.dispatch(addTodo('Learn about reducers'));
+store.dispatch(addTodo('Learn about store'));
+store.dispatch(toggleTodo(0));
+store.dispatch(toggleTodo(1));
+store.dispatch(setVisibilityFilter(VisibilityFilters.SHOW_COMPLETED));
+
+unsubscribe();
 
 ReactDOM.render(
   <React.StrictMode>
