44fdaf65e0dcb626e48953de2b1c8fe6620bcf09
motokikasai <kmomoky@hotmail.com>
2020-10-21 16:16:05 +0200

    Set up reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,25 +1,31 @@
 import { combineReducers } from 'redux';
-
-const initialState = {
-  count: 0,
-};
-
-const countReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case 'INCREMENT':
-      return {
-        count: state.count + 1,
-      };
-    case 'DECREMENT':
-      return {
-        count: state.count - 1,
-      };
-    default:
-      return state;
-  }
-};
+import {
+  ADD_TODO,
+  SET_VISIBILITY_FILTER,
+  TOGGLE_TODO,
+  VisibilityFilters,
+} from '../actions/actions';
 
 const { SHOW_ALL } = VisibilityFilters;
+
+// const initialState = {
+//   count: 0,
+// };
+
+// const countReducer = (state = initialState, action) => {
+//   switch (action.type) {
+//     case 'INCREMENT':
+//       return {
+//         count: state.count + 1,
+//       };
+//     case 'DECREMENT':
+//       return {
+//         count: state.count - 1,
+//       };
+//     default:
+//       return state;
+//   }
+// };
 
 function visibilityFiler(state = SHOW_ALL, action) {
   switch (action.type) {
@@ -30,9 +36,35 @@ function visibilityFiler(state = SHOW_ALL, action) {
   }
 }
 
-const rootReducer = combineReducers({
-  countReducer,
-  //   todoReducer,
+function todos(state = [], action) {
+  switch (action.type) {
+    case ADD_TODO:
+      return [
+        ...state,
+        {
+          text: action.text,
+          completed: false,
+        },
+      ];
+    case TOGGLE_TODO:
+      return state.map((todo, index) => {
+        if (index === action.index) {
+          return { ...todo, completed: !todo.completed };
+        }
+        return todo;
+      });
+    default:
+      return state;
+  }
+}
+
+// const rootReducer = combineReducers({
+//   countReducer,
+// });
+
+const todoApp = combineReducers({
+  visibilityFiler,
+  todos,
 });
 
-export default rootReducer;
+export default todoApp;
