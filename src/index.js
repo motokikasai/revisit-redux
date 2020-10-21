d411884290d47149202a02841d7f6a22a5d9d925
motokikasai <kmomoky@hotmail.com>
2020-10-21 17:44:32 +0200

    Apply all the basic logic

--- src/index.js
+++ src/index.js
@@ -6,26 +6,27 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import todoApp from './reducers';
 
-import {
-  addTodo,
-  toggleTodo,
-  setVisibilityFilter,
-  VisibilityFilters,
-} from './actions/actions';
-
 const store = createStore(todoApp);
 
-const unsubscribe = store.subscribe(() => console.log(store.getState()));
+// TEST WITHOUT REACT---
+// import {
+//   addTodo,
+//   toggleTodo,
+//   setVisibilityFilter,
+//   VisibilityFilters,
+// } from './actions/actions';
+
+// const unsubscribe = store.subscribe(() => console.log(store.getState()));
 
 // console.log(store.getState());
-store.dispatch(addTodo('Learn about actions'));
-store.dispatch(addTodo('Learn about reducers'));
-store.dispatch(addTodo('Learn about store'));
-store.dispatch(toggleTodo(0));
-store.dispatch(toggleTodo(1));
-store.dispatch(setVisibilityFilter(VisibilityFilters.SHOW_COMPLETED));
+// store.dispatch(addTodo('Learn about actions'));
+// store.dispatch(addTodo('Learn about reducers'));
+// store.dispatch(addTodo('Learn about store'));
+// store.dispatch(toggleTodo(0));
+// store.dispatch(toggleTodo(1));
+// store.dispatch(setVisibilityFilter(VisibilityFilters.SHOW_COMPLETED));
 
-unsubscribe();
+// unsubscribe();
 
 ReactDOM.render(
   <React.StrictMode>
